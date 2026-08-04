23f42ccf2b66eb5f6f52f7df1f296dc982ceb6fa
TokisakiKurumi <212392849+xcy616@users.noreply.github.com>
2026-08-05 02:15:09 +0800

    fix: 修复相册照片页图片挤在一起的问题
    - Photo.vue 中 el-col 被包在 el-checkbox-group 内部，不是 el-row 的
      直接子元素，导致 flex 网格布局失效，照片纵向堆叠挤在一起
    - 调整结构：每个 el-col 内各放一个 checkbox-group，恢复横向网格排列
    - 补充样式让 checkbox 撑满网格单元，照片正常显示并保留勾选功能
    - 已验证：照片按每行 6 张横向排列，间距正常，勾选/删除功能不受影响
    
    日期：2026-08-05

--- aurora-vue/aurora-admin/src/views/album/Photo.vue
+++ aurora-vue/aurora-admin/src/views/album/Photo.vue
@@ -44,8 +44,8 @@
     </div>
     <el-row class="photo-container" :gutter="10" v-loading="loading">
       <el-empty v-if="photos.length == 0" description="暂无照片" />
-      <el-checkbox-group v-model="selectphotoIds" @change="handleCheckedPhotoChange">
-        <el-col :md="4" v-for="item of photos" :key="item.id">
+      <el-col :md="4" v-for="item of photos" :key="item.id">
+        <el-checkbox-group v-model="selectphotoIds" @change="handleCheckedPhotoChange">
           <el-checkbox :label="item.id">
             <div class="photo-item">
               <div class="photo-opreation">
@@ -62,8 +62,8 @@
               <div class="photo-name">{{ item.photoName }}</div>
             </div>
           </el-checkbox>
-        </el-col>
-      </el-checkbox-group>
+        </el-checkbox-group>
+      </el-col>
     </el-row>
     <el-pagination
       :hide-on-single-page="true"
@@ -443,6 +443,16 @@ export default {
   position: relative;
   cursor: pointer;
   margin-bottom: 1rem;
+}
+.photo-container .el-checkbox {
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+}
+.photo-container .el-checkbox__label {
+  flex: 1;
+  min-width: 0;
+  padding: 0;
 }
 .photo-img {
   width: 100%;
